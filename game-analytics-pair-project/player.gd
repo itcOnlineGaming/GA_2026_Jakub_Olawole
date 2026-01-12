7a07b7ee66bf29289cf5ec7fe86954232512b443
Jakub-Stepien1 <ninjadeluxepl@gmail.com>
2026-01-12 14:08:15 +0000

    pickups increase player speed, time survived is displayed

--- game-analytics-pair-project/player.gd
+++ game-analytics-pair-project/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 var speed = 300.0
 var score = 0
-
+var time_survived = 0
 
 func _physics_process(delta: float) -> void:
 	var direction := Vector2(
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	)
 	
-	print(score)
+	$CanvasLayer/PanelContainer/Label.text = " Time Survived: " + str(time_survived) + " seconds "
 	
 	velocity = direction * speed
 
@@ -19,3 +19,9 @@ func _physics_process(delta: float) -> void:
 
 func increase_score() -> void:
 	score = score + 1
+	speed = 300 + 20 * score
+
+
+func _on_timer_timeout() -> void:
+	time_survived = time_survived + 1
+	pass # Replace with function body.
